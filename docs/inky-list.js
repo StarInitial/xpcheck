ccbeb939cf7aa51f89b9a2fa6b6d4c60b92788c4
StarInitial <1623418595@qq.com>
2022-07-27 15:37:10 +0800

    Signed-off-by: StarInitial <1623418595@qq.com>

---
+++ docs/inky-list.js
@@ -0,0 +1,17 @@
+var InkyList = {
+    "inky-text-list":[
+        {
+            "id":0,
+            "url":"xpcheck",
+            "name":"深渊凝视着你",
+            "update":"2022/06/20",
+            "status":"OK",
+            "author":[],
+            "program-author":[
+                {
+                    "name":"Star"
+                }
+            ]
+        }
+    ]
+};
